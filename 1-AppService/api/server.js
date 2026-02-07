d4c4836d77183d813a22cb77cd9cd4172db54b76
Sylvain Riquen <sriquen@vaudoise.ch>
2026-02-07 21:00:58 +0100

    feat: integrate Azure Monitor OpenTelemetry and add robust error handling for API calls

--- 1-AppService/api/server.js
+++ 1-AppService/api/server.js
@@ -1,13 +1,22 @@
+const { useAzureMonitor } = require("@azure/monitor-opentelemetry");
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 
-app.get('/', async (req, res) => {
-  // Call https://jsonplaceholder.typicode.com/posts and get the 10 first posts
-  const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json());
-  const first10Posts = posts.slice(0, 10);
+useAzureMonitor();
 
-  res.json({ message: 'Result of the call to https://jsonplaceholder.typicode.com/posts', posts: first10Posts });
+app.get('/', async (req, res) => {
+  try {
+    const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json());
+    const first10Posts = posts.slice(0, 10);
+
+    console.log('Successfully called https://jsonplaceholder.typicode.com/posts');
+
+    res.json({ message: 'Result of the call to https://jsonplaceholder.typicode.com/posts', posts: first10Posts });
+  } catch (error) {
+    console.error('Error calling https://jsonplaceholder.typicode.com/posts', error);
+    res.status(500).json({ message: 'Error calling https://jsonplaceholder.typicode.com/posts' });
+  }
 });
 
 app.listen(port, () => {
