58a5cd3ce7a61e09abbf388846350255f33fa353
Sylvain Riquen <sriquen@vaudoise.ch>
2026-02-07 19:36:42 +0100

    feat: Add App Service infrastructure scripts for networking and authentication.

---
+++ 1-AppService/infra/2-privateendpoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+# Add subnet + nsg for private endpoint
+# Add the default NSG rules to block all inbound traffic
---
+++ 1-AppService/infra/3-routevnetoutbound.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+# Add subnet + nsg for vnet integration
+# Add the default NSG rules to block all outbound traffic simulating a default FW rule
